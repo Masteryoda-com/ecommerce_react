d0359da4fd334f2b6a94b858df320d06a3f964fa
Masteryoda-com <saikumarsaikumar070@gmail.com>
2023-02-03 20:40:17 +0530

    list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import ItemList from './components/ItemList';
+
 import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <ItemList />
+      <ItemList/>
+      
     </div>
   );
 }
